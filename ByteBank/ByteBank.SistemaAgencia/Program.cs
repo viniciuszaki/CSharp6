63124831e2c72eb61e2f7a47ea5161329ac5e71d
viniciuszaki <viniciuszaki@gmail.com>
2021-11-03 21:04:04 -0300

    Propriedade Length e sobrecargas de IndexOf

--- ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -16,6 +16,30 @@ namespace ByteBank.SistemaAgencia
             // pagina?argumentos
             //12345678
 
+            //moedaOrigem=real&moedaDestino=dolar
+            //                             |
+            //             ----------------
+
+
+            string palavra = "moedaOrigem=real&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino";
+
+            int indice = palavra.IndexOf(nomeArgumento);
+            Console.WriteLine(indice);
+
+            Console.WriteLine("Tamanho da string nomeArgumento: " + nomeArgumento.Length);
+
+            Console.WriteLine(palavra);
+
+            Console.WriteLine(palavra.Substring(indice));
+            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length + 1));
+            Console.ReadLine();
+
+
+
+
+
+            // Testando o IsNullOrEmpty
             string textoVazio = "";
             string textoNulo = null;
             string textoQualquer = "hsauihsauisahiusah";
